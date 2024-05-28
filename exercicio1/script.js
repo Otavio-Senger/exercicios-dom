74c12b624e151a3c0c372bd3ceb109a4d1d29ebe
Igor-Bandeira08 <igordelimabandeira03@gmail.com>
2024-05-28 16:54:29 -0300

    primeiro

---
+++ exercicio1/script.js
@@ -0,0 +1,10 @@
+
+const paragrafo1 = document.getElementById("paragrafo1");
+const botao = document.getElementById("button1");
+ botao.addEventListener ("click", function() {
+    paragrafo1.textContent = "botão clicado"; 
+ });
+
+
+
+
