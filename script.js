1fb329ac8d9f8fcb5c972694679e85f74fe678bd
Otavio-Senger <166336227+Otavio-Senger@users.noreply.github.com>
2024-05-30 19:08:09 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var paragrafo = document.getElementById("campo");
-
-var botao1 = document.getElementById("botao1");
-
-botao1.addEventListener ("click", function() {
-     document.getElementById("paragrafomostra").innerText = paragrafo.value;
-});
