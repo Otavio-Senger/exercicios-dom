8bf8d87e08d7884e778b5515a0619a590b673967
Otavio-Senger <senger.otavio@gmail.com>
2024-05-30 21:56:00 -0300

    exercicio 3

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var botao = document.getElementById("botao");
+    var lista = document.getElementById("lista");
+
+botao.addEventListener("click", function(){
+    var item = document.createElement("li");
+    item.textContent = "novo item";
+    lista.appendChild(item);
+});
+});
