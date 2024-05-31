ff4fed8f51810e5fbe70454fbd22947ad87dff90
Otavio-Senger <166336227+Otavio-Senger@users.noreply.github.com>
2024-05-31 15:05:32 -0300

    Delete exercicio3 directory

--- exercicio3/script.js
+++
@@ -1,9 +0,0 @@
-var botao = document.getElementById("botao1");
-
-
-botao.addEventListener("click", function(){
-    var paragrafo = document.getElementById("contador");
-    var pegarvalor = parseInt(paragrafo.textContent);
-    pegarvalor++;
-    paragrafo.textContent = pegarvalor;
-})
